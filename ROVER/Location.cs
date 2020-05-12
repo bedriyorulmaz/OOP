c0951a87ec06a4bc963a387a1ca741ae306160ca
bedriyorulmaz <50769889+bedriyorulmaz@users.noreply.github.com>
2020-05-13 00:31:43 +0300

    Add files via upload

---
+++ ROVER/Location.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ROVER
+{   public enum Direction
+    {                 //Reference value is defined for directions.
+        N=1,
+        E=2,
+        S=3,
+        W=4
+    } 
+    public interface ILocation
+    {
+        void Start(List<int> edgepoint, string orders);
+    }
+    public class Location : ILocation
+    {
+        public void Start(List<int> edgepoint, string orders)
+        {
+            foreach (var order in orders)
+            {
+                switch (order)
+                {
+                    case 'M':
+                        this.SameDirection();
+                        break;
+                    case 'L':
+                        this.Left();
+                        break;                      //Movement orders have been defined.
+                    case 'R':
+                        this.Right();
+                        break;
+                    default:
+                        Console.WriteLine($"You entired wrong {order}");
+                        break;
+                }
+
+                if (this.X < 0 || this.X > edgepoint[0] || this.Y < 0 || this.Y > edgepoint[1])
+                {
+                    throw new Exception($"You entered a value outside the planet's boundary.");
+                }
+            }
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Direction Direction { get; set; }
+
+        public Location()
+        {
+            X = Y = 0;                           //the lower-left coordinates are assumed to be 0, 0.
+            Direction = Direction.N;
+        }
+
+        private void Left()
+        {
+            if (this.Direction == Direction.N) { this.Direction = Direction.W; }
+            else if (this.Direction == Direction.S) { this.Direction = Direction.E; }
+            else if (this.Direction == Direction.E) { this.Direction = Direction.N; }
+            else if (this.Direction == Direction.W) { this.Direction = Direction.S; }
+            
+        }
+
+        private void Right()
+        {
+            if (this.Direction == Direction.N) { this.Direction = Direction.E; }
+            else if (this.Direction == Direction.S) { this.Direction = Direction.W; }
+            else if (this.Direction == Direction.E) { this.Direction = Direction.S; }
+            else if (this.Direction == Direction.W) { this.Direction = Direction.N; }
+        }
+
+        private void SameDirection()
+        {
+            if (this.Direction == Direction.N) { this.Y += 1; }
+            else if (this.Direction == Direction.S) { this.Y -=1; }
+            else if (this.Direction == Direction.E) { this.X += 1; }
+            else if (this.Direction == Direction.W) { this.X -= 1; }
+        }
+
+
+    }
+}
+         
---
+++ ROVER/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ROVER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number of rover");
+            int rovernumber = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("the upper-right coordinates of the plateau:");
+            var edgepoint = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToList();
+
+            List<Location> locations = new List<Location>();
+            for (int i = 0; i < rovernumber; i++)
+            {
+                Location location = new Location();
+                locations.Add(location);
+            }
+
+
+            for (int i = 0; i < rovernumber; i++)
+            {
+
+
+                Console.WriteLine($" \n {i + 1}. rover start location:");
+
+                var startlocation = Console.ReadLine().Trim().Split(' ');
+
+                locations[i].X = Convert.ToInt32(startlocation[0]); //convert string to integer 
+                locations[i].Y = Convert.ToInt32(startlocation[1]);
+                locations[i].Direction = (Direction)Enum.Parse(typeof(Direction), startlocation[2].ToUpper());  //get enum value by string
+
+
+                Console.WriteLine($"{i + 1}. rover orders for rover's movement:");
+                var orders = Console.ReadLine().ToUpper();
+
+
+
+
+                try
+                {
+                    locations[i].Start(edgepoint, orders);
+                    Console.WriteLine($"{i + 1}. rover new location {locations[i].X} {locations[i].Y} {locations[i].Direction}");
+
+                }                                                                                       //that is control If a location outside 
+                catch (Exception exception) { Console.WriteLine(exception.Message); }                   //the border is entered, it will give an alert.
+
+            }
+        }
+    }
+}
